737d1eb580efe1ade640ef04dc83f4987fb0690b
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-07-07 20:33:18 +0530

    Refactor loop to break on mango using index reference

--- week1/python-1/practice/loops2.py
+++ week1/python-1/practice/loops2.py
@@ -2,7 +2,7 @@ fruits=["apple","orange","mango","grapes"]
 
 for i in fruits:
     print(i)
-    # if(i==fruits[2]):
-    #     break
-    if(i=="mango"):
+    if(i==fruits[2]):
         break
+    # if(i=="mango"):
+    #     break
